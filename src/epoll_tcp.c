d82e6fa0a9c8dae97bdf77b87cd77aef585eb7e8
pudding186 <pudding_186@163.com>
2020-02-03 20:54:28 +0800

    fix warning

--- src/epoll_tcp.c
+++ src/epoll_tcp.c
@@ -580,7 +580,7 @@ epoll_tcp_socket* _epoll_tcp_manager_alloc_socket(epoll_tcp_manager* mgr, unsign
 
 void _epoll_tcp_manager_free_socket(epoll_tcp_manager* mgr, epoll_tcp_socket* sock_ptr)
 {
-    void** ptr_mem_unit = memory_unit_get_sign((void*)sock_ptr);
+    void** ptr_mem_unit = memory_unit_get_sign(sock_ptr);
     if (!memory_unit_check_sign(mgr->socket_pool, ptr_mem_unit))
     {
         CRUSH_CODE();
@@ -2614,7 +2614,8 @@ epoll_tcp_manager* create_net_tcp(pfn_on_establish func_on_establish, pfn_on_ter
 
     for (unsigned int i = 0; i < max_socket_num; i++)
     {
-        _main_thread_free(mgr->socket_pool, arry_socket_ptr[i]);
+        void** ptr_mem_unit = memory_unit_get_sign(arry_socket_ptr[i]);
+        _main_thread_free(mgr->socket_pool, ptr_mem_unit);
     }
 
     free(arry_socket_ptr);
